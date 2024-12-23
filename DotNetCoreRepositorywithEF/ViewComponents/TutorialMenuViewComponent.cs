f9ec4b11e634488d9541c1a45688f559ab14fbc9
zaintahiri <zain.tahiri940@gmail.com>
2024-12-23 10:56:49 +0500

    Here view have worked with ViewComponents.
    1-add view component folder inside the rootdirectory of the project and Components folder then inside it  TutorialMenu folder
    
    2- Add TutorialMenuViewComponent.cs fiel inside the ViewComponent Folder
    
    using DotNetCoreRepositorywithEF.Repository;
    using Microsoft.AspNetCore.Mvc;
    
    namespace DotNetCoreRepositorywithEF.ViewComponents
    {
        public class TutorialMenuViewComponent:ViewComponent
        {
            private readonly ITutorialRepository _tutorial;
            public TutorialMenuViewComponent(ITutorialRepository tutorial)
            {
                _tutorial = tutorial;
            }
    
            public IViewComponentResult Invoke()
            {
               var tutorials= _tutorial.GetAllTutorials().OrderBy(x=>x.Name);
                return View(tutorials);
            }
        }
    }
    
    3-Add Default.cshtml file inside TutorialMenu folder.
    
    @model IEnumerable<Tutorial>;
    <li class="nav-item dropdown">
        <a class="nav-link dropdown-toggle" id="navbarDarkDropdownMenuLink" role="button"
           data-bs-toggle="dropdown" aria-expanded="false">
            Tutorials
        </a>
        <ul class="dropdown-menu" aria-labelledby="navbarDarkDropdownMenuLink">
            @foreach (var tutorial in Model)
            {
                <li><a class="dropdown-item" href="#">@tutorial.Name</a></li>
            }
        </ul>
    </li>
    
    4-Add code inside _Layout
     @await Component.InvokeAsync("TutorialMenu")

---
+++ DotNetCoreRepositorywithEF/ViewComponents/TutorialMenuViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using DotNetCoreRepositorywithEF.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotNetCoreRepositorywithEF.ViewComponents
+{
+    public class TutorialMenuViewComponent:ViewComponent
+    {
+        private readonly ITutorialRepository _tutorial;
+        public TutorialMenuViewComponent(ITutorialRepository tutorial)
+        {
+            _tutorial = tutorial;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+           var tutorials= _tutorial.GetAllTutorials().OrderBy(x=>x.Name);
+            return View(tutorials);
+        }
+    }
+}
